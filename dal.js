01eb02e64c3c01ebf3a80f687a321ac87b273037
Marysabel Mejia <102928783+marysabelmejia@users.noreply.github.com>
2023-01-10 23:44:18 -0500

    Update dal.js

--- dal.js
+++ dal.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
-const url         = 'mongodb://localhost:27017';
+const url         = process.env.PORT || 'mongodb://localhost:27017';
 let db            = null;
  
 // connect to mongo
