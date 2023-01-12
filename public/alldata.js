b1af21ebf7980c5fca28f8e9609a746c13ab3162
Marysabel Mejia <102928783+marysabelmejia@users.noreply.github.com>
2023-01-12 15:24:52 -0500

    Update alldata.js

--- public/alldata.js
+++ public/alldata.js
@@ -19,7 +19,7 @@ function AllData(){
   React.useEffect(() => {
       
       // fetch all accounts from API
-      fetch('http://badbank-mm.vercel.app/account/all')
+      fetch('https://badbank-mm.vercel.app/account/all')
           .then((response) => response.json())
           .then(data => {
               console.log('here');
