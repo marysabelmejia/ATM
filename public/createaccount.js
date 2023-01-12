cc23282ec9765d0e2cdfe4281ebc21835735f7be
Marysabel Mejia <102928783+marysabelmejia@users.noreply.github.com>
2023-01-12 15:25:23 -0500

    Update createaccount.js

--- public/createaccount.js
+++ public/createaccount.js
@@ -26,7 +26,7 @@ function CreateAccount() {
     alert("Password must be 8 characters long.");
     return;
     }}
-    fetch(`http://badbank-mm.vercel.app/account/create/${name}/${email}/${password}`)
+    fetch(`https://badbank-mm.vercel.app/account/create/${name}/${email}/${password}`)
     .then(response => response.json())
     .then(data => console.log('this comes from the backend', data))
     
